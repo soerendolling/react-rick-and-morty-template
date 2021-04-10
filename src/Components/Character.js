18f715f8663dd8e89ca2c7b6a6326def1b3c2b57
Soeren Dolling <soeren.dolling@gmail.com>
2021-04-10 12:16:54 +0200

    Add character status background

--- src/Components/Character.js
+++ src/Components/Character.js
@@ -1,6 +1,18 @@
 export default function Character({ characterData }) {
+  let classForStatus;
+
+  if (characterData.status === "Alive") {
+    classForStatus = "character--alive";
+  } else if (characterData.status === "Dead") {
+    classForStatus = "character--dead";
+  } else {
+    classForStatus = "character--unknown";
+  }
+
+  console.log(classForStatus);
+
   return characterData.map((characterInfo) => (
-    <article className="character-box">
+    <article className={`character-box ${classForStatus}`}>
       <img
         className="character-box__image"
         src={characterInfo.image}
